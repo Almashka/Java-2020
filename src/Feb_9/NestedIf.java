0feeb5dda093ea582b5876fb6930bd166b265e9a
TestProjectForGitHub <almashkakz@yahoo.com>
2020-02-09 12:38:29 -0600

    My first Java files

---
+++ src/Feb_9/NestedIf.java
@@ -0,0 +1,14 @@
+package Feb_9;
+
+public class NestedIf {
+    public static void main(String[] args) {
+        int x = 1, y = -4;
+        if(x == 1){
+            System.out.println("OK");
+            if(x > 0){
+                System.out.println("Hello");
+            }
+            System.out.println("OK OK");
+        }
+    }
+}
