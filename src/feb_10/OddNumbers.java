3d3ffc8e5862441576a99a7d9f66f96b80e893c0
TestProjectForGitHub <almashkakz@yahoo.com>
2020-02-10 19:58:52 -0600

    odd numbers

---
+++ src/feb_10/OddNumbers.java
@@ -0,0 +1,11 @@
+package feb_10;
+
+public class OddNumbers {
+    public static void main(String[] args) {
+        for(int i = 1; i < 101; i++){
+            if(i%2 != 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
