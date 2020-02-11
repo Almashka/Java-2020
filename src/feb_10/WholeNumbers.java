4e9955d7edb5aaf1f5a3d2d2c605d322131c2056
TestProjectForGitHub <almashkakz@yahoo.com>
2020-02-10 19:25:35 -0600

    for loop (whole numbers)

---
+++ src/feb_10/WholeNumbers.java
@@ -0,0 +1,9 @@
+package feb_10;
+
+public class WholeNumbers {
+    public static void main(String[] args) {
+        for(int i = 100; i < 1001; i++){
+            System.out.println(i);
+        }
+    }
+}
