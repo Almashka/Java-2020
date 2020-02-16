bb3dba14411cad4dccedb528ee04a0e5b61fd02e
TestProjectForGitHub <almashkakz@yahoo.com>
2020-02-16 16:51:26 -0600

    lower case letters

--- src/feb_16/LowerCaseLetters.java
+++ src/feb_16/LowerCaseLetters.java
@@ -4,7 +4,7 @@ public class LowerCaseLetters {
     public static void main(String[] args) {
         String sentence = "I never Forget to push my Codes to GitHub";
         for(int i = 0; i < sentence.length(); i++){
-            if(sentence.charAt(i) >= 97 && sentence.charAt(i) <= 122){
+            if((sentence.charAt(i) >= 97 && sentence.charAt(i) <= 122) || sentence.charAt(i) == 32){
                 System.out.print(sentence.charAt(i) + " ");
             }
         }
