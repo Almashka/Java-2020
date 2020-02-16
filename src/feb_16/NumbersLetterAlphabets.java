d9317361a9a4e72fbc7b6977f364a9930cf982d4
TestProjectForGitHub <almashkakz@yahoo.com>
2020-02-16 12:19:12 -0600

    nested for loops

---
+++ src/feb_16/NumbersLetterAlphabets.java
@@ -0,0 +1,15 @@
+package feb_16;
+
+public class NumbersLetterAlphabets {
+    public static void main(String[] args) {
+        char letters = 'a';
+        for(int i = 1; i < 6; i++){
+            System.out.print(i + "\t");
+            for(int j = 0; j < 4; j++){
+                System.out.print(letters + "\t");
+                letters++;
+            }
+            System.out.println();
+        }
+    }
+}
