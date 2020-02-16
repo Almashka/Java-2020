681a704741efb657a35ffbc4839223760813fb6f
TestProjectForGitHub <almashkakz@yahoo.com>
2020-02-16 15:47:04 -0600

    find vowels (while loop)

---
+++ src/feb_16/VowelsWithWhile.java
@@ -0,0 +1,14 @@
+package feb_16;
+
+public class VowelsWithWhile {
+    public static void main(String[] args) {
+        String sentence = "Sunday, fun day!";
+        int index = 0;
+        while(index < sentence.length()) {
+            if (sentence.charAt(index) == 'a' || sentence.charAt(index) == 'e' || sentence.charAt(index) == 'i' || sentence.charAt(index) == 'o' || sentence.charAt(index) == 'u') {
+                System.out.print(sentence.charAt(index) + " ");
+            }
+            index++;
+        }
+    }
+}
