64f6494505cef761b685a2dc966efad1bc812c52
TestProjectForGitHub <almashkakz@yahoo.com>
2020-02-23 14:47:45 -0600

    printing numbers

--- src/feb_23/NumbersPrinting.java
+++ src/feb_23/NumbersPrinting.java
@@ -5,7 +5,7 @@ public class NumbersPrinting {
         int number = 1;
         for(int i = 0; i < 3; i++){
             for(int j = 0; j < 5; j++){
-                System.out.print(number + " ");
+                System.out.print(number + "\t");
                 number++;
             }
             System.out.println();
