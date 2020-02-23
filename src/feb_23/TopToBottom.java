208460b8d32104bf24e1ceb7dca764b50a811ecc
TestProjectForGitHub <almashkakz@yahoo.com>
2020-02-23 13:05:54 -0600

    updated top to bottom

--- src/feb_23/TopToBottom.java
+++ src/feb_23/TopToBottom.java
@@ -1,10 +1,14 @@
 package feb_23;
 
 public class TopToBottom {
+    public static int smth(){
+        return 20;
+    }
     public static void main(String[] args) {
         topToBottom("Hello");
     }
     public static void topToBottom(String word){
+        System.out.println(smth());
         for(int i = 0; i < word.length(); i++){
             System.out.println(word.charAt(i));
         }
