f091671cdc5a5c646fc2884bf624c6f8f2f0eb26
TestProjectForGitHub <almashkakz@yahoo.com>
2020-02-24 19:15:17 -0600

    character removing using String

--- src/feb_24/CharRemovingTest.java
+++ src/feb_24/CharRemovingTest.java
@@ -3,5 +3,7 @@ package feb_24;
 public class CharRemovingTest {
     public static void main(String[] args) {
         CharRemoving.removeChar("Java is fun", 'i');
+        System.out.println();
+        System.out.println(CharRemoving.removeCharStr("Hello", 'e'));
     }
 }
