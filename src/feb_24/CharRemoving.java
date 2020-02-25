77d45be3b31637e5279a73d67ff5f1c643832c49
TestProjectForGitHub <almashkakz@yahoo.com>
2020-02-24 19:09:53 -0600

    character removing

--- src/feb_24/CharRemoving.java
+++ src/feb_24/CharRemoving.java
@@ -4,7 +4,7 @@ public class CharRemoving {
     public static void removeChar(String text, char letter) {
         for(int i = 0; i < text.length(); i++){
             if(text.charAt(i) != letter){
-                System.out.println(text.charAt(i));
+                System.out.print(text.charAt(i));
             }
         }
     }
